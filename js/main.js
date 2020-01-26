4ac3bc86b18590303ac0cdaa2b5754caa5098aa6
frontend90 <leon44173@gmail.com>
2020-01-26 19:52:06 +0200

    верстка

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ $('.title .btn').click(function () {
   $('.header .overlay').css("transition", "1s");
   $('.header .overlay').toggleClass('active');
 });
+/*************         slick       ************/
+$('.blog-slider').slick({
+  centerMode: true,
+  centerPadding: '160px',
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
