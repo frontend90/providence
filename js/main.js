e4d984df6af33c1bb309de1d7ef366ddb26a7cd6
frontend90 <leon44173@gmail.com>
2020-01-28 11:19:30 +0200

    верстка

--- js/main.js
+++ js/main.js
@@ -6,7 +6,23 @@ $('.title .btn').click(function () {
 /*************         slick       ************/
 $('.blog-slider').slick({
   centerMode: true,
-  centerPadding: '160px',
-  slidesToShow: 1,
-  slidesToScroll: 1
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: true,
+  speed: 1000
 });
+
+/****************      pricing card    **************/
+$('.individual').click(function(){
+$('.individual').addClass('active');
+$('.starter').addClass('active');
+$('.company').removeClass('active');
+  $('.pro').removeClass('active');
+});
+
+$('.company').click(function(){
+  $('.company').addClass('active');
+  $('.pro').addClass('active');
+  $('.individual').removeClass('active');
+$('.starter').removeClass('active');
+  });
